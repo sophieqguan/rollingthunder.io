8faa3fbe5f97dea17ca84979f7cf294cb99ac4e7
sophieguan <sophie.qin.guan@gmail.com>
2025-01-17 18:39:58 -0800

    modified some CORS thing bane of my existence

--- amplify/functions/rollerHandler/index.py
+++ amplify/functions/rollerHandler/index.py
@@ -3,11 +3,6 @@ import json
 
 def handler(event, context):
     return {
-        "headers": {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS'
-        },
         "statusCode": 200,
         "body": json.dumps({
             "message": "Hello World",
